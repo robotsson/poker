0bee382edbd9785400aa2a1872677f3233d6e63f
ppettert <game-baked-jogging@duck.com>
2024-11-01 23:36:26 +0100

    code cleanup

--- poker.js
+++ poker.js
@@ -16,7 +16,7 @@ const deck = [
 ];
 
 
-function getRandomInt(max) {
+function getRandomInt( max ) {
     return Math.floor(Math.random() * max);
 }
 
@@ -24,18 +24,16 @@ function getRandomInt(max) {
    used to return a set of 5 unique numbers
    between 0 and 51
 */
-let getRandomSet = function( nbr, max, uniq )
+let getRandomSet = function( count, max )
 {
-    let nbrs = uniq ? new Set() : [];
-    let add = e => uniq ? nbrs.add( e ) : nbrs.push( e );
-    let l = () => uniq ? nbrs.size : nbrs.length;
+    let numbers = new Set();
 
-    while( l() < nbr )
+    while( numbers.size < count )
     {
-        add( getRandomInt( max ) );
+       numbers.add( getRandomInt( max ) );
     }
 
-    return nbrs;
+    return numbers;
 };
 
 /* remove the last character in card string (i.e "5S", "10C" .. ) to get rank */
@@ -49,59 +47,79 @@ const rank = ["A","K","Q","J","10","9","8","7","6","5","4","3","2"];
 
 
 /* compare two card deck entries */
-function compare( a, b  )
+function compare( a, b )
 {
     // deck[a] and deck[b] contains
     // a string made up of rank followed by suit (S,C,H,D) 
     // removing suit to compare it by looking up the position
     // of the card's rank in the rank array 
-    let ar = getrank(a ); // deck[a].slice( 0, -1 );
-    let br = getrank( b); // deck[b].slice( 0, -1 );
+    let ar = getrank(a ); 
+    let br = getrank( b);
 
     return rank.indexOf(ar)-rank.indexOf(br);
 }
 
+/* last character in card is the suit/color */
 function getsuit( card )
 {
     // last character in string
     return deck[card].at(-1);
 }
 
-
+/* a sorted histogram is returned from the hand */
 function getsortedhistogram( hand )
 {
     let cardranks = hand.map( getrank );
     //console.log( cardranks );
     let histogram = new Map();
 
-    for( let i = 0; i < cardranks.length; i++ )
-    {
-        let currentcard = cardranks[i];
+    // this loop creates and unsorted histogram from hand
+    // for( let i = 0; i < cardranks.length; i++ )
+    // {
+    //     let currentcard = cardranks[i];
 
-        if( ! histogram.has( currentcard ) )
-        { 
-            let samecards = cardranks.filter( (x) => currentcard === x );
-            histogram.set( cardranks[i], samecards.length );
+    //     if( ! histogram.has( currentcard ) )
+    //     { 
+    //         // counts how many of the current card is in the hand
+    //         let samecards = cardranks.filter( (x) => currentcard === x );
+    //         histogram.set( cardranks[i], samecards.length );
+    //     }
+    // }
+
+    // rewritten by Claude.ai
+    for ( let card of cardranks ) 
+    {
+        if ( ! histogram.has(card) ) 
+        {
+            const frequency = cardranks.filter( x => x === card ).length;
+            histogram.set( card, frequency );
         }
     }
-    
-
 
     // Converts histogram map to array of (key,value)-pairs using spread, 
     // then sort the pairs according to the value, and creates a new map 
     // out of this array
-    let sortedhistogram = new Map([...histogram]
-                            .sort( ( [ , a_v], [ , b_v] ) => b_v - a_v ));
-      
+    // let sortedhistogram = new Map([...histogram]
+    //                        .sort( ( [ , a_v], [ , b_v] ) => b_v - a_v ));
+
+    // rewritten by Claude.ai
+    let sortedhistogram = new Map(
+        Array.from( histogram ).sort( ( a, b ) => b[1] - a[1] )
+    );
+                            
     return( sortedhistogram );
 }
 
+/* detects if the hand is a straight, see explanation of the
+   algorithm in findhands() */
 function is_straight( hand )
 {
     let first = getrank( hand[0] );
     let last = getrank( hand[4] );
     
-    let firstint = 13 -rank.indexOf(first);
+    // rank array A,K,...,5,4,3,2
+    // A has index 0, 2 has index 13
+    let firstint = 13 - rank.indexOf(first);
     let lastint = 13 - rank.indexOf(last);
 
     if( ( firstint - lastint ) === 4 )
@@ -112,18 +130,21 @@ function is_straight( hand )
     {
         // Check for "wheel"
         let second = getrank( hand[1] );
-        let secondint = 13 - rank.indexOf(second);
-
-        // console.log( "wheel: "+secondint +" "+ lastint )
-
-        if( ( secondint - lastint ) === 3 )
+        if( second === "5" )
         {
-            return true;
-        }
-    } 
+            let secondint = 13 - rank.indexOf(second);
+            // console.log( "wheel: "+secondint +" "+ lastint )
+
+            if( ( secondint - lastint ) === 3 )
+            {
+                return true;
+            }
+        } 
+    }   
 
 }
 
+/* detects if all the cards have the same suit / color */
 function is_flush( hand )
 {
     let a = hand.map( getsuit );
@@ -132,47 +153,37 @@ function is_flush( hand )
         && ( a[2] == a[3] ) && ( a[3] == a[4] );
 }
 
+/* detects winning hands and returns the enthusiastic name for it */
 function findhands( hand )
 {
     // console.log("findhands");
     /*   
-    Algorithm found here:
-    https://nsayer.blogspot.com/2007/07/algorithm-for-evaluating-poker-hands.html
+        Histogram based algorithm found here:
+        https://nsayer.blogspot.com/2007/07/algorithm-for-evaluating-poker-hands.html
 
-    First take a histogram of the card ranks. 
-    That is, for each rank in the hand, count how often it appears. 
-    
-    Sort the histogram by the count backwards (high values first).
-    If the histogram counts are 4 and 1, then the hand is quads.
-    
-    If the histogram counts are 3 and a 2, then the hand is a boat.
-    
-    If the histogram counts are 3, 1 and 1, then the hand is a set.
-    
-    If the histogram counts are 2, 2 and 1, then the hand is two pair.
-    
-    If the histogram has 4 ranks in it, then the hand is one pair.
-    
-    Next, check to see if the hand is a flush. You do this by iterating 
-    through the cards to see if the suit of a card is the same as its neighbor.
-    If not, then the hand is not a flush. Don't return a result yet, 
-    just note whether or not it's a flush.
-    
-    Next, check for straights. Do this by sorting the list of cards. 
-    Then subtract the rank of the bottom card from the top card. 
-    If you get 4, it's a straight. At this point, you must also check 
-    either for the wheel or for broadway, depending on whether your sort 
-    puts the ace at the top or bottom. I would expect most folks to put 
-    the ace at the top of the sort, since it is usually a high card. 
-    So to check for the wheel, check to see if the top card is an ace 
-    and the 2nd to top card is a 5. If so, then it is the wheel.
-    
-    If the hand is a straight and a flush, it's a straight-flush. 
-    Otherwise if it's one or the other, you can return that.
+        if the histogram contains..
+        2 cards, frequency 4 and 1 => four of a kind
+        2 cards, frequency 3 and 2 => full house
+        3 cards, frequency 3, 1 and 1 => three of a kind
+        3 cards, frequency 2, 2 and 1 => two pairs
+        4 cards => one pair
 
+        flush, just check the end character of every card.
     
-    If we haven't matched the hand by now, it's High Card.
+        straights are checked by having a sorted list of cards,
+        and subtracting rank of the lowest card from the highest,
+        if the difference is 4, you have a straight
+
+        special cases to detect if it's AKQJ10 or A5432 (broadway
+        and wheel).
+
+        if it's a straight and card 1 is A and card 2 is K, we have
+        a royal flush if it's in the same suit(color).
+
+        the wheel is detectected when card 1 is A, card 2 is 5 and 
+        the difference between it and the lowest card is 3.   
     */
+
     let histogram = getsortedhistogram( hand );
 
     console.table( [...histogram] );
@@ -236,7 +247,7 @@ function findhands( hand )
     return "";
 }
 
-let hand = Array.from( getRandomSet( 5, 52, true ) );
+let hand = Array.from( getRandomSet( 5, 52 ) );
 
 let fusk1 = [0,1,2,3,4];
 let fusk2 = [0,1,2,4,5];
@@ -248,7 +259,7 @@ let sortedhand = hand.sort(compare);
 let handstr = findhands( sortedhand );
 
 // special case for A5432, "wheel", looks nicer to put 
-// A last
+// A last in the displayed hand
 if( handstr === "STRAIGHT!" || handstr === "STRAIGHT FLUSH!" )
 {
     //console.log("test for wheel");
